b5211a925b246f1de2a6edfaa323addacab5cf82
Josh <>
2023-11-01 07:10:47 -0400

    Added a right click menu to exit the application. Did not rebuilf the .exe, it is still V1.2

--- pineconetimer.py
+++ pineconetimer.py
@@ -1,14 +1,16 @@
+#Imports
 import tkinter  as tk 
 from tkinter import *
 from webbrowser import *
 from time import strftime
 
+#Font details
+my_fonttime=('arial',40,'bold')
+my_fontdate=('arial',15,'bold')
+
+#This manages window movement
 lastClickX = 0
 lastClickY = 0
-
-my_fonttime=('arial',40,'bold') # display size and style
-my_fontdate=('arial',15,'bold') # display size and style
-
 def SaveLastClickPos(event):
     global lastClickX, lastClickY
     lastClickX = event.x
@@ -18,19 +20,16 @@ def Dragging(event):
     x, y = event.x - lastClickX + root.winfo_x(), event.y - lastClickY + root.winfo_y()
     root.geometry("+%s+%s" % (x , y))
     
-
+#Setup for the window
 root = tk.Tk()
 root.overrideredirect(True)
 root.attributes('-topmost', True)
-#root.geometry("400x400+500+300")
 root.bind('<Button-1>', SaveLastClickPos)
 root.bind('<B1-Motion>', Dragging)
 root.configure(background='black')
-root.title("PineconeTimer V1.1")
-#img = PhotoImage(file='pineconetimer.ico')
-#root.tk.call('wm', 'iconphoto', root._w, img)
+root.title("PineconeTimerV1.3")
 
-
+#Time and Date logic
 def my_time(): 
     l1=tk.Label(root,font=my_fonttime,bg='black',fg='#0f0',anchor='n')
     l1.grid(row=0,column=0,padx=0,pady=0)
@@ -43,6 +42,20 @@ def my_date():
     l2.grid(row=1,column=0,padx=0,pady=0)
     date_string = strftime(' %A %B %d %Y ')
     l2.config(text=date_string)
+    
+#Right click menu
+m = Menu(root, tearoff=0)
+m.add_command(label="Exit", command=root.destroy)
+def do_popup(event): 
+    try: 
+        m.tk_popup(event.x_root, event.y_root) 
+    finally: 
+        m.grab_release() 
+  
+root.bind("<Button-3>", do_popup) 
+
+
+#runs the code
 my_time()
 my_date()
 root.mainloop()
