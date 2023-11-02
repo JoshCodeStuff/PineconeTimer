44e8f2fc9a04b3eb7f74cd3b98db3b95a946ecac
Josh <>
2023-11-01 21:50:51 -0400

    V1.5 - Fixed a memory issue which broke the clock after some time. cleaned up some comments and added a right click menu for exiting as well.

--- pineconetimer.py
+++ pineconetimer.py
@@ -4,11 +4,16 @@ from tkinter import *
 from webbrowser import *
 from time import strftime
 
+showtime = 1
+showdate = 1
+
 #Font details
 my_fonttime=('arial',40,'bold')
 my_fontdate=('arial',15,'bold')
+timecolor = '#0f0'
+datecolor = '#0f0'
 
-#This manages window movement
+#These manage window movement
 lastClickX = 0
 lastClickY = 0
 def SaveLastClickPos(event):
@@ -20,32 +25,35 @@ def Dragging(event):
     x, y = event.x - lastClickX + root.winfo_x(), event.y - lastClickY + root.winfo_y()
     root.geometry("+%s+%s" % (x , y))
     
-#Setup for the window
+#Setup for the window ans text
 root = tk.Tk()
 root.overrideredirect(True)
 root.attributes('-topmost', True)
 root.bind('<Button-1>', SaveLastClickPos)
 root.bind('<B1-Motion>', Dragging)
 root.configure(background='black')
-root.title("PineconeTimerV1.4")
-
+root.title("PineconeTimerV1.5")
+timelabel=Label(root,font=my_fonttime,bg='black',fg=timecolor,anchor='n')
+datelabel=Label(root,font=my_fontdate,bg='black',fg=datecolor,anchor='s')
+        
 #Time and Date logic
-def my_time(): 
-    l1=tk.Label(root,font=my_fonttime,bg='black',fg='#0f0',anchor='n')
-    l1.grid(row=0,column=0,padx=0,pady=0)
+def timeHandler(): 
+    timelabel.grid(row=0,column=0,padx=0,pady=0)
     time_string = strftime(' %H:%M:%S ')
-    l1.config(text=time_string)
-    l1.after(1000,my_time) # time delay of 1000 milliseconds
-
-def my_date():
-    l2=tk.Label(root,font=my_fontdate,bg='black',fg='#0f0',anchor='s')
-    l2.grid(row=1,column=0,padx=0,pady=0)
+    timelabel.config(text=time_string) 
+    timelabel.after(1000, timeHandler) # time delay of 1000 milliseconds
+def dateHandler():
+    datelabel.grid(row=1,column=0,padx=0,pady=0)
     date_string = strftime(' %A, %B %d, %Y ')
-    l2.config(text=date_string)
-    
+    datelabel.config(text=date_string)
+    datelabel.after(10000, dateHandler)
 #Right click menu
 m = Menu(root, tearoff=0)
+#m.add_checkbutton(label='Toggle Time', onvalue=0, offvalue=1)
+#m.add_checkbutton(label='Toggle Date', onvalue=0, offvalue=1)
 m.add_command(label="Exit", command=root.destroy)
+
+
 def do_popup(event): 
     try: 
         m.tk_popup(event.x_root, event.y_root) 
@@ -56,6 +64,6 @@ root.bind("<Button-3>", do_popup)
 
 
 #runs the code
-my_time()
-my_date()
+timeHandler()
+dateHandler()
 root.mainloop()
